7dac674f72999178f55f443b300b01f9c4291c15
Matt Sternke <msternke11@gmail.com>
2021-01-29 07:43:28 -0500

    Developing html, adding NLP analysis

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/local-race')
+def local_race():
+    return render_template('local-race.html')
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
